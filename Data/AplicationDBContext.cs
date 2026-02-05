20f142f09ce5e5020638dfe91d05c7acb8dd7a44
JhonatanEducapp <jdiaz@sistemaeducapp.com>
2026-02-05 17:54:34 -0600

    Se ha defenido las propiedades para que EF pueda crear la base de datos.

--- Data/AplicationDBContext.cs
+++ Data/AplicationDBContext.cs
@@ -1,8 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using SystemBase.Models;
 
 namespace SystemBase.Data;
 
-public class AplicationDBContext : DbContext
+public class AplicationDbContext : DbContext
 {
+    public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+    {
+    }
     
+    public DbSet<users> users { get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<users>(users =>
+        {
+            //definimos primarykey
+            users.HasKey(u => u.id);
+            users.Property(u => u.userName).IsRequired().HasMaxLength(15);
+            users.Property(u=>u.password).IsRequired().HasMaxLength(16);
+            users.Property(u => u.name).IsRequired().HasMaxLength(50);
+            users.Property(u=>u.app).HasMaxLength(50);
+            users.Property(u=>u.apm).HasMaxLength(50);
+            users.Property(u =>u.created).IsRequired();
+            users.Property(u=>u.status).HasDefaultValue(true);
+        });
+    }
 }       
---
+++ Models/users.cs
@@ -0,0 +1,17 @@
+namespace SystemBase.Models;
+
+public class users
+{
+    public users()
+    {
+        status = true;
+    }
+    public int id { get; set; }
+    public string userName { get; set; }
+    public string password {get; set;}
+    public string name { get; set;}
+    public string app {get; set; }
+    public string apm {get; set; } 
+    public DateTimeOffset created { get; set;}
+    public bool status { get; set; }
+}
